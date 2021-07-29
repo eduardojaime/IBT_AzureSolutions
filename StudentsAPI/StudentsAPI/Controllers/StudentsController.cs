bbc04fa0dfa718930e58469edfe1df87850ca9e4
Eduardo Jaime <eduardojaime86@gmail.com>
2021-07-29 17:35:13 -0400

    Students API project with .NET 5

---
+++ StudentsAPI/StudentsAPI/Controllers/StudentsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentsAPI.Data;
+using StudentsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // Dependency Injection
+        private ApplicationDbContext _context;
+
+        public StudentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Create Endpoints
+        [HttpGet]
+        public IActionResult ListAllStudents()
+        {
+            var students = _context.Students.ToList<Student>();
+
+            return new JsonResult(students);
+        }
+    }
+}
---
+++ StudentsAPI/StudentsAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StudentsAPI.Models;
+
+namespace StudentsAPI.Data
+{
+    // extend from DbContext base class
+    public class ApplicationDbContext : DbContext
+    {
+        // Add dbsets
+        public DbSet<Student> Students { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+    }
+}
